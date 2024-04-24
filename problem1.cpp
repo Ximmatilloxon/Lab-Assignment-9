3254b119caeec95c70cb38954a8a8bd05b218e61
Ximmatilloxon Musayev <x.musayev@newuzbekistanuniversity.uz>
2024-04-24 11:23:02 +0500

    p1

--- problem1.cpp
+++ problem1.cpp
@@ -1,1 +1,16 @@
+#include "iostream"
+#include "vector"
+using namespace std;
 
+int sum(vector<int>& v){
+    int s = 0;
+    for(int i:v) s += i;
+    return s;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<vector<int>> adjList{{12, 25}, {12, 13, 15}, {13, 5, 2}, {15, 5, 13}, {5, 5}, {2, 13, 25}};
+    cout << sum(adjList[n]);
+}
