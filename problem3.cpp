0ab1e99056becf5c7eb50c17a79cce55c4ed8258
Ximmatilloxon Musayev <x.musayev@newuzbekistanuniversity.uz>
2024-04-24 11:41:49 +0500

    p3

--- problem3.cpp
+++ problem3.cpp
@@ -1,1 +1,34 @@
+#include "iostream"
+#include "vector"
+#include "queue"
+using namespace std;
 
+void bfs(vector<vector<int>>& adjList, int start, vector<int>& distance){
+    distance[start] = 0;
+    queue<int> q;
+    q.push(start);
+    while (!q.empty()){
+        int u = q.front(); q.pop();
+        for (int vertex: adjList[u]) {
+            if(distance[vertex] == -1){
+                distance[vertex] = distance[u] + 1;
+                q.push(vertex);
+            }
+        }
+    }
+}
+
+int main(){
+    int n, e;
+    cin >> n >> e;
+    vector<vector<int>> adjList(n);
+    int node1, node2;
+    for (int i = 0; i < e; ++i) {
+        cin >> node1 >> node2;
+        adjList[node1].push_back(node2);
+        adjList[node2].push_back(node1);
+    }
+    vector<int> d(n, -1);
+    bfs(adjList, 0, d);
+    for(auto dis:d) cout << dis << " ";
+}
